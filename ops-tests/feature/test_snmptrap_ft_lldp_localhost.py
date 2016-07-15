5321e5925bb8efd4bab51c9ccf7ddee1693da7d4
avinash <avinash.varma@hpe.com>
2016-07-15 03:51:52 -0700

    new:chg: Adding snmptrap script localhost
    TG-1333
    
    Change-Id: I301b33bc5d48f1f8880090532626a2665f32d614
    Signed-off-by: avinash <avinash.varma@hpe.com>

--- ops-tests/feature/test_snmptrap_ft_lldp_localhost.py
+++ ops-tests/feature/test_snmptrap_ft_lldp_localhost.py
@@ -17,32 +17,28 @@
 ##########################################################################
 
 """
-OpenSwitch Test for lldp SNMP traps with host
+OpenSwitch Test for lldp SNMP traps on localhost
+for tier 2
 Author: Avinash(avinash.varma@hpe.com)
-Description: Tests for snmp traps in LLDP with host
+Description: Tests for snmp traps in LLDP
 """
 
-from pytest import mark
+# from pytest import mark
 from time import sleep
 
 TOPOLOGY = """
-# +-------+
-# |       |     +-------+     +-------+
-# |  hs1  <----->  ops1  <----->  ops2  |
-# |       |     +-------+     +-------+
-# +-------+
+#
+# +-------+     +-------+
+# | ops1  <----->  ops2 |
+# +-------+     +-------+
+#
 
 # Nodes
 [type=openswitch name="OpenSwitch 1"] ops1
 [type=openswitch name="OpenSwitch 2"] ops2
-[type=oobmhost name="Host 1" image="openswitch/snmpd:latest"] hs1
-
-# Ports
-[force_name=oobm] ops1:sp1
 
 # Links
-ops1:if01 -- ops2:if01
-ops1:sp1 -- hs1:1
+ops1:1 -- ops2:1
 """
 
 
@@ -51,49 +47,51 @@ def config_lldp(ops):
         ctx.lldp_enable()
 
 
-def config_trapreceiver(ops, hs):
-    opsmgmtip = '10.10.10.4/24'
-    trapserverip = '10.10.10.5'
+def disable_lldp(ops):
+    with ops.libs.vtysh.Configure() as ctx:
+        ctx.no_lldp_enable()
+
+
+def config_trapreceiver(ops):
+    opsmgmtipsub = '10.10.10.4/24'
+    opsmgmtip = '10.10.10.4'
 
     with ops.libs.vtysh.ConfigInterfaceMgmt() as ctx:
-        ctx.ip_static(opsmgmtip)
+        ctx.ip_static(opsmgmtipsub)
 
     with ops.libs.vtysh.Configure() as ctx:
-        ctx.snmp_server_host_trap_version(trapserverip, 'v1')
+        ctx.snmp_server_host_trap_version(opsmgmtip, 'v1')
 
     with ops.libs.vtysh.Configure() as ctx:
-        ctx.snmp_server_host_trap_version(trapserverip, 'v2c')
+        ctx.snmp_server_host_trap_version(opsmgmtip, 'v2c')
+
+    with ops.libs.vtysh.Configure() as ctx:
+        ctx.snmp_server_host_inform_version(opsmgmtip, 'v2c')
 
     ops.libs.vtysh.show_running_config()
 
-    hs('echo "authCommunity log,execute,net public" > \
-        /etc/snmp/snmptrapd.conf')
-
-    hs("ifconfig")
-    hs("ifconfig eth0 10.10.10.5/24 up")
-
-    sleep(10)
-
-    ping = hs("ping -c 1 10.10.10.4")
-    assert "1 packets transmitted, 1 packets received" in ping
-
-    hs('snmptrapd -Lftemp.txt')
+    ops('systemctl stop snmptrapd', shell="bash")
+    ops('echo "authCommunity log,execute,net public" > \
+        /etc/snmp/snmptrapd.conf', shell="bash")
+    ops('snmptrapd -Lftemp.txt', shell="bash")
 
 
-def verify_lldpremtableschange(ops1, ops2, hs1):
-    ops1p1 = ops1.ports['if01']
-    ops2p1 = ops2.ports['if01']
+def verify_lldpremtableschange(ops1, ops2):
+    ops1p1 = ops1.ports['1']
+    ops2p1 = ops2.ports['1']
 
     # This is main trap oid which is present in the trap output
     remtableschangeoid = 'iso.0.8802.1.1.2.0.0.1'
     # This is the insert counter which is incremented when lldp is enabled.
     # Verify if the count has increased in the trap output
     remtablesinsertoid = 'iso.0.8802.1.1.2.1.2.2'
+    remtablesdeletesoid = 'iso.0.8802.1.1.2.1.2.3'
+    remtablesageoutsoid = 'iso.0.8802.1.1.2.1.2.5'
 
     config_lldp(ops1)
     config_lldp(ops2)
 
-    config_trapreceiver(ops1, hs1)
+    config_trapreceiver(ops1)
 
     with ops1.libs.vtysh.ConfigInterface(ops1p1) as ctx:
         ctx.no_shutdown()
@@ -117,29 +115,44 @@ def verify_lldpremtableschange(ops1, ops2, hs1):
     ops1.libs.vtysh.show_lldp_neighbor_info(ops1p1)
     ops2.libs.vtysh.show_lldp_neighbor_info(ops2p1)
 
-    ret = hs1('cat temp.txt')
+    with ops2.libs.vtysh.ConfigInterface(ops1p1) as ctx:
+        ctx.no_shutdown()
+
+    sleep(40)
+    ops1.libs.vtysh.show_lldp_neighbor_info(ops1p1)
+    ops1.libs.vtysh.show_lldp_neighbor_info(ops2p1)
+    disable_lldp(ops2)
+
+    sleep(130)
+
+    ret = ops1('cat temp.txt', shell="bash")
     trapcount = 0
-    incount = 0
+    incount = deletecount = ageoutcount = 0
     for line in ret.split('\n'):
         if remtableschangeoid in line:
             trapcount = trapcount + 1
         if remtablesinsertoid + ' = Gauge32: 1' in line:
             incount = incount + 1
+        if remtablesdeletesoid + ' = Gauge32: 1' in line:
+            deletecount = deletecount + 1
+        if remtablesageoutsoid + ' = Gauge32: 1' in line:
+            ageoutcount = ageoutcount + 1
 
-    assert trapcount == 4, "The total number of traps \
-                received doesn't match expected number"
-    assert incount == 2, "The total number of inserts \
-                doesn't match expected value"
+    assert trapcount == 12, "Total number of traps received \
+            is not equal to expected count"
+    assert incount >= 3, "Total number of insert received \
+            is not equal to expected count"
+    assert deletecount >= 3, "Total number of deletes received \
+            is not equal to expected count"
+    assert ageoutcount >= 3, "Total number of ageouts received \
+            is not equal to expected count"
 
 
-@mark.platform_incompatible(['ostl'])
 def test_snmptrap_ft_lldp(topology, step):
     ops1 = topology.get('ops1')
     ops2 = topology.get('ops2')
-    hs1 = topology.get('hs1')
 
     assert ops1 is not None
     assert ops2 is not None
-    assert hs1 is not None
 
-    verify_lldpremtableschange(ops1, ops2, hs1)
+    verify_lldpremtableschange(ops1, ops2)
